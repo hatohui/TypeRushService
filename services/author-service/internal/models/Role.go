9c9a8dfe65d1abdf04a9e00eeecb1332800423d3
ThienTM27 <thien.tm2727@gmail.com>
2025-12-05 14:26:30 +0700

    chore: delete unused authen and author service

--- services/author-service/internal/models/Role.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Role struct {
-  RoleID      uint           `gorm:"primaryKey;autoIncrement" json:"role_id"`
-  Name        string         `gorm:"size:100;not null;unique" json:"name"`
-	Permissions []Permission   `gorm:"many2many:role_permission;" json:"permissions,omitempty"`
-}
